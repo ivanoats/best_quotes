c8bc458ca29d675de81d24c902d39350ca722cc2
Ivan Storck <ivanoats@gmail.com>
2013-10-29 22:02:46 -0700

    exception handling

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,13 @@
 class QuotesController < Rulers::Controller
   def a_quote
     "There is nothing either good or bad" +
-      "but thinking makes it so."
+      "but thinking makes it so." +
+      "\n<pre>\n#{env}\n</pre>"
   end
+
+  def exception
+    raise BadError, "It's a bad one!"
+  end
+
+  class ::BadError < Exception; end
 end
