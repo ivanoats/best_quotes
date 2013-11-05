910e4d8b54d068cce6f59c23955314f84085ce94
Ivan Storck <ivanoats@gmail.com>
2013-11-04 22:47:25 -0800

    ch 3 before exercises

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source 'https://rubygems.org'
 
-gem 'rulers'
+gem 'rulers', path: '../rulers'
--- config/application.rb
+++ config/application.rb
@@ -3,10 +3,8 @@ require 'rulers'
 $LOAD_PATH << File.join(File.dirname(__FILE__),
                        "..","app",
                        "controllers")
-require "quotes_controller"
 
 module BestQuotes
   class Application < Rulers::Application
-
   end
 end
